a8d77c48a7a5ed041246c84e360960fdf17582c1
bs080402270-ctrl <bs080402270@gmail.com>
2026-09-21 13:46:12 +0500

    Add Unity Android build pipeline

---
+++ Unity/Assets/Editor/AndroidBuild.cs
@@ -0,0 +1,45 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TimeLoopDetective.Editor
+{
+    public static class AndroidBuild
+    {
+        [MenuItem("Time Loop Detective/Build Android APK")]
+        public static void BuildAndroid()
+        {
+            const string scenePath = "Assets/Scenes/Main.unity";
+            Directory.CreateDirectory("Assets/Scenes");
+            var scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+            EditorSceneManager.SaveScene(scene, scenePath);
+
+            PlayerSettings.productName = "Time Loop Detective";
+            PlayerSettings.companyName = "ZetaRank";
+            PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.zetarank.timeloopdetective");
+            PlayerSettings.defaultInterfaceOrientation = UIOrientation.Portrait;
+            PlayerSettings.Android.bundleVersionCode = 11;
+            PlayerSettings.bundleVersion = "1.2.0-unity";
+            PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel26;
+            PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
+            PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARM64 | AndroidArchitecture.ARMv7;
+
+            Directory.CreateDirectory("Builds/Android");
+            var options = new BuildPlayerOptions {
+                scenes = new[] { scenePath },
+                locationPathName = "Builds/Android/TimeLoopDetective-Unity.apk",
+                target = BuildTarget.Android,
+                options = BuildOptions.Development
+            };
+            var report = BuildPipeline.BuildPlayer(options);
+            if (report.summary.result != BuildResult.Succeeded)
+                throw new System.Exception("Android build failed: " + report.summary.result);
+            Debug.Log("Android APK built: " + options.locationPathName);
+        }
+
+        public static void BuildAndroidFromCI() => BuildAndroid();
+    }
+}
